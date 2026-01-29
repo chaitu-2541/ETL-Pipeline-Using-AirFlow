96bb78c9a27460e6300018e3aca8c50b081fc73d
Chaitanya-Dev <76583086+chaitu-2541@users.noreply.github.com>
2026-01-29 22:21:35 +0530

    Loading master data

---
+++ load_master_data_csv_to_postgres.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+from utils.db import engine
+
+def load_master_data():
+
+    # --- Locate master_data.csv ---
+    def find_file(filename, search_path):
+        for root, dirs, files in os.walk(search_path):
+            if filename in files:
+                return os.path.join(root, filename)
+        raise FileNotFoundError(f"{filename} not found in {search_path}")
+
+    try:
+        csv_path = find_file("master_data.csv",search_path="/opt/airflow/dags/db_config")
+        df = pd.read_csv(csv_path)
+        df.to_sql("master_data", engine, schema = 'bronze', if_exists="replace", index=False)
+        print("Master_Data ingested successfully.")
+    except FileNotFoundError as e:
+        print(e)
